9328906d89de5697fb3cc046ebce50a0d8eb1131
daniel-568 <da568@georgetown.edu>
2022-11-12 12:36:24 -0500

    updated helperFunctions, schemaGenerator, and dbConnection to TypeScript

--- server/db/dbConnection.js
+++ server/db/dbConnection.js
@@ -1,121 +1,187 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
 require('dotenv').config();
-const { Pool } = require('pg');
-
-// create a func that accepts URI input and exports/returns query obj that's being exported (line 21)
-// then export said func
-// since return value is query obj, we can import it into helperFuncs
-
-function dbInstance( inputURI ) {
-  if (inputURI.includes('postgres')) this.dbType = 'PostgreSQL';
-  this.pool = new Pool({connectionString: inputURI});
-  this.query = (text, params, callback) => {
-    // console.log('executed query', text);
-    return this.pool.query(text, params, callback);
-  }
-};
-
-// TODO: add catch blocks into func methods
-// return line 18 on helper functions
+var Pool = require('pg').Pool;
+// const { stringify } = require('querystring');
+// const { GlobalServerError, dbConstructor } = require('../types');
+/*
+create a func that accepts URI input and exports/returns query obj that's being exported (line 21), then export said func
+since return value is query obj, we can import it into helperFuncs
+*/
+function dbInstance(inputURI) {
+    var _this = this;
+    if (inputURI.includes('postgres'))
+        this.dbType = 'PostgreSQL';
+    this.pool = new Pool({ connectionString: inputURI });
+    this.query = function (text, params, callback) {
+        // console.log('executed query', text);
+        return _this.pool.query(text, params, callback);
+    };
+}
+;
 // Given a database, return all table names in an array
-dbInstance.prototype.queryTables = async function () {
-  try {
-    if (this.dbType === 'PostgreSQL') {
-      const tablesQuery = `
-      SELECT table_name
-      FROM information_schema.tables
-      WHERE table_schema='public'
-      AND table_type='BASE TABLE';`;
-      const { rows } = await this.query(tablesQuery);
-      // console.log(rows);
-      return rows;
-    }
-  } catch (err) {
-    return {
-      log: `dbConnection.js: ERROR: ${err}`,
-    }
-  }
+dbInstance.prototype.queryTables = function () {
+    return __awaiter(this, void 0, void 0, function () {
+        var tablesQuery, rows, err_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 3, , 4]);
+                    if (!(this.dbType === 'PostgreSQL')) return [3 /*break*/, 2];
+                    tablesQuery = "\n      SELECT table_name\n      FROM information_schema.tables\n      WHERE table_schema='public'\n      AND table_type='BASE TABLE';";
+                    return [4 /*yield*/, this.query(tablesQuery)];
+                case 1:
+                    rows = (_a.sent()).rows;
+                    return [2 /*return*/, rows];
+                case 2: return [3 /*break*/, 4];
+                case 3:
+                    err_1 = _a.sent();
+                    return [2 /*return*/, {
+                            log: "dbConnection.js.queryTables: ERROR: " + err_1,
+                            status: 400,
+                            message: { err: 'An error occured' }
+                        }];
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
 };
-
 // Given a table name, return all columns of that table in an array of objects
-dbInstance.prototype.queryColumns = async function (tableName) {
-  try {
-    const query = `
-    SELECT column_name, data_type
-    FROM information_schema.columns
-    WHERE table_name = '${tableName}';
-    `;
-    const { rows } = await this.query(query);
-    // console.log(rows);
-    return rows;
-  } catch (err) {
-    return {
-      log: `dbConnection.js: ERROR: ${err}`,
-    }
-  }
+dbInstance.prototype.queryColumns = function (tableName) {
+    return __awaiter(this, void 0, void 0, function () {
+        var query, rows, err_2;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 2, , 3]);
+                    query = "\n    SELECT column_name, data_type\n    FROM information_schema.columns\n    WHERE table_name = '" + tableName + "';\n    ";
+                    return [4 /*yield*/, this.query(query)];
+                case 1:
+                    rows = (_a.sent()).rows;
+                    return [2 /*return*/, rows];
+                case 2:
+                    err_2 = _a.sent();
+                    return [2 /*return*/, {
+                            log: "dbConnection.js.queryColumns: ERROR: " + err_2,
+                            status: 400,
+                            message: { err: 'An error occured' }
+                        }];
+                case 3: return [2 /*return*/];
+            }
+        });
+    });
 };
-
 // Returns query for all primary keys of a database
-dbInstance.prototype.queryPKey = async function () {
-  try {
-    const query = `
-    SELECT conrelid::regclass AS table_name,
-    conname AS primary_key, 
-    pg_get_constraintdef(oid) 
-    FROM   pg_constraint 
-    WHERE  contype = 'p'
-    AND    connamespace = 'public'::regnamespace   
-    ORDER  BY conrelid::regclass::text, contype DESC; 
-    `;
-    const { rows } = await this.query(query);
-    // console.log(rows);
-    return rows;
-  } catch (err) {
-    return {
-      log: `dbConnection.js: ERROR: ${err}`,
-    }
-  }
+dbInstance.prototype.queryPKey = function () {
+    return __awaiter(this, void 0, void 0, function () {
+        var query, rows, err_3;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 2, , 3]);
+                    query = "\n    SELECT conrelid::regclass AS table_name,\n    conname AS primary_key, \n    pg_get_constraintdef(oid) \n    FROM   pg_constraint \n    WHERE  contype = 'p'\n    AND    connamespace = 'public'::regnamespace   \n    ORDER  BY conrelid::regclass::text, contype DESC; \n    ";
+                    return [4 /*yield*/, this.query(query)];
+                case 1:
+                    rows = (_a.sent()).rows;
+                    return [2 /*return*/, rows];
+                case 2:
+                    err_3 = _a.sent();
+                    return [2 /*return*/, {
+                            log: "dbConnection.js.queryPKey: ERROR: " + err_3,
+                            status: 400,
+                            message: { err: 'An error occured' }
+                        }];
+                case 3: return [2 /*return*/];
+            }
+        });
+    });
 };
-
 // Returns query for all 
-dbInstance.prototype.queryFKeys = async function () {
-  try {
-    const query = `
-    SELECT conrelid::regclass AS table_name, 
-    conname AS foreign_key, 
-    pg_get_constraintdef(oid) 
-    FROM   pg_constraint 
-    WHERE  contype = 'f' 
-    AND    connamespace = 'public'::regnamespace   
-    ORDER  BY conrelid::regclass::text, contype DESC;
-    `;
-    const { rows } = await this.query(query)
-    // console.log(rows);
-    return rows;
-  } catch (err) {
-    return {
-      log: `dbConnection.js: ERROR: ${err}`,
-    }
-  }
+dbInstance.prototype.queryFKeys = function () {
+    return __awaiter(this, void 0, void 0, function () {
+        var query, rows, err_4;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 2, , 3]);
+                    query = "\n    SELECT conrelid::regclass AS table_name, \n    conname AS foreign_key, \n    pg_get_constraintdef(oid) \n    FROM   pg_constraint \n    WHERE  contype = 'f' \n    AND    connamespace = 'public'::regnamespace   \n    ORDER  BY conrelid::regclass::text, contype DESC;\n    ";
+                    return [4 /*yield*/, this.query(query)];
+                case 1:
+                    rows = (_a.sent()).rows;
+                    return [2 /*return*/, rows];
+                case 2:
+                    err_4 = _a.sent();
+                    return [2 /*return*/, {
+                            log: "dbConnection.js.queryFKeys: ERROR: " + err_4,
+                            status: 400,
+                            message: { err: 'An error occured' }
+                        }];
+                case 3: return [2 /*return*/];
+            }
+        });
+    });
 };
-
-
-dbInstance.prototype.queryTableLayout = async function (tableName) {
-  try {
-    const query = `
-    SELECT table_name, column_name, is_nullable, data_type
-    FROM information_schema.columns
-    WHERE table_schema = 'public' AND table_name = 'people'
-    `;
-    const { rows } = await this.query(fKeysQuery)
-    // console.log(rows);
-    return rows;
-  } catch (err) {
-    return {
-      log: `dbConnection.js: ERROR: ${err}`,
-    }
-  }
+dbInstance.prototype.queryTableLayout = function (tableName) {
+    return __awaiter(this, void 0, void 0, function () {
+        var query, rows, err_5;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 2, , 3]);
+                    query = "\n    SELECT table_name, column_name, is_nullable, data_type\n    FROM information_schema.columns\n    WHERE table_schema = 'public' AND table_name = '" + tableName + "'\n    ";
+                    return [4 /*yield*/, this.query(query)];
+                case 1:
+                    rows = (_a.sent()).rows;
+                    return [2 /*return*/, rows];
+                case 2:
+                    err_5 = _a.sent();
+                    return [2 /*return*/, {
+                            log: "dbConnection.js.queryTableLayout: ERROR: " + err_5,
+                            status: 400,
+                            message: { err: 'An error occured' }
+                        }];
+                case 3: return [2 /*return*/];
+            }
+        });
+    });
 };
-
-// then pass retrieveTables into parseTables (which takes evaluated result of retrieveTables)
-
+var sWAPI = new dbInstance('postgres://eitysjmj:At82GArc1PcAD4nYgBoAODn0-XvBYo-A@peanut.db.elephantsql.com/eitysjmj');
+console.log(sWAPI);
+console.log('hi');
 module.exports = dbInstance;
---
+++ server/db/dbConnection.ts
@@ -0,0 +1,130 @@
+import { GlobalServerError, dbConstructor } from '../types';
+require('dotenv').config();
+const { Pool } = require('pg');
+import { stringify } from 'querystring';
+
+// const { stringify } = require('querystring');
+// const { GlobalServerError, dbConstructor } = require('../types');
+
+/* 
+create a func that accepts URI input and exports/returns query obj that's being exported (line 21), then export said func
+since return value is query obj, we can import it into helperFuncs
+*/
+
+function dbInstance(this: dbConstructor, inputURI: string ): void {
+  if (inputURI.includes('postgres')) this.dbType = 'PostgreSQL';
+  this.pool = new Pool({connectionString: inputURI});
+  this.query = (text: string, params?: object, callback?: Function): Object => {
+    // console.log('executed query', text);
+    return this.pool.query(text, params, callback);
+  }
+};
+
+// Given a database, return all table names in an array
+dbInstance.prototype.queryTables = async function ():Promise<[] | GlobalServerError>  {
+  try {
+    if (this.dbType === 'PostgreSQL') {
+      const tablesQuery = `
+      SELECT table_name
+      FROM information_schema.tables
+      WHERE table_schema='public'
+      AND table_type='BASE TABLE';`;
+      const { rows } = await this.query(tablesQuery);
+      return rows;
+    }
+  } catch (err: unknown) {
+    return {
+      log: `dbConnection.js.queryTables: ERROR: ${err}`,
+      status: 400,
+      message: {err: 'An error occured'}
+    }
+  }
+};
+
+// Given a table name, return all columns of that table in an array of objects
+dbInstance.prototype.queryColumns = async function (tableName: string):Promise<[] | GlobalServerError> {
+  try {
+    const query = `
+    SELECT column_name, data_type
+    FROM information_schema.columns
+    WHERE table_name = '${tableName}';
+    `;
+    const { rows } = await this.query(query);
+    return rows;
+  } catch (err: unknown) {
+    return {
+      log: `dbConnection.js.queryColumns: ERROR: ${err}`,
+      status: 400,
+      message: {err: 'An error occured'}
+    }
+  }
+};
+
+// Returns query for all primary keys of a database
+dbInstance.prototype.queryPKey = async function ():Promise<[] | GlobalServerError> {
+  try {
+    const query = `
+    SELECT conrelid::regclass AS table_name,
+    conname AS primary_key, 
+    pg_get_constraintdef(oid) 
+    FROM   pg_constraint 
+    WHERE  contype = 'p'
+    AND    connamespace = 'public'::regnamespace   
+    ORDER  BY conrelid::regclass::text, contype DESC; 
+    `;
+    const { rows } = await this.query(query);
+    return rows;
+  } catch (err: unknown) {
+    return {
+      log: `dbConnection.js.queryPKey: ERROR: ${err}`,
+      status: 400,
+      message: {err: 'An error occured'}
+    }
+  }
+};
+
+// Returns query for all 
+dbInstance.prototype.queryFKeys = async function ():Promise<[] | GlobalServerError> {
+  try {
+    const query = `
+    SELECT conrelid::regclass AS table_name, 
+    conname AS foreign_key, 
+    pg_get_constraintdef(oid) 
+    FROM   pg_constraint 
+    WHERE  contype = 'f' 
+    AND    connamespace = 'public'::regnamespace   
+    ORDER  BY conrelid::regclass::text, contype DESC;
+    `;
+    const { rows } = await this.query(query)
+    return rows;
+  } catch (err: unknown) {
+    return {
+      log: `dbConnection.js.queryFKeys: ERROR: ${err}`,
+      status: 400,
+      message: {err: 'An error occured'}
+    }
+  }
+};
+
+
+dbInstance.prototype.queryTableLayout = async function (tableName: string):Promise<[] | GlobalServerError> {
+  try {
+    const query = `
+    SELECT table_name, column_name, is_nullable, data_type
+    FROM information_schema.columns
+    WHERE table_schema = 'public' AND table_name = '${tableName}'
+    `;
+    const { rows } = await this.query(query)
+    return rows;
+  } catch (err: unknown) {
+    return {
+      log: `dbConnection.js.queryTableLayout: ERROR: ${err}`,
+      status: 400,
+      message: {err: 'An error occured'}
+    }
+  }
+};
+
+const sWAPI = new (dbInstance as any)('postgres://eitysjmj:At82GArc1PcAD4nYgBoAODn0-XvBYo-A@peanut.db.elephantsql.com/eitysjmj');
+
+module.exports = dbInstance;
