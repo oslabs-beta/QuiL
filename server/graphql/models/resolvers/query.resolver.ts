a92c9ad1ad3496126452025a9facfdf9139c8f29
Andres Jaramillo <andresjaramillo@Andress-Mac-mini.local>
2022-11-19 07:34:17 -0800

    adding new dev

--- server/graphql/models/resolvers/query.resolver.ts
+++ server/graphql/models/resolvers/query.resolver.ts
@@ -10,6 +10,7 @@ import {
   makeResolverStrings,
 } from '../../../resolverGenerator';
 import { generateSchemas } from '../../../schemaGenerator';
+import { stringify } from 'querystring';
 
 export const Query = {
   getAllData: async (_: any, args: ArgType): Promise<QuiLData> => {
@@ -28,6 +29,25 @@ export const Query = {
       schemas: queryString,
     };
   },
+};
+
+export const userCreateQuery = {
+  Query: {
+    newUser(_: any, obj: newUser) {
+      return obj; // returns an obj that contains em usr pw
+    },
+  },
+};
+type newUser = {
+  email: String;
+  username: String;
+  password: String;
+};
+
+const fakeUser = {
+  email: 'andres@quil.com',
+  username: 'QuiL',
+  password: 'QL4u',
 };
 
 // resolvers: nodes.map((node: nodeShape) =>
