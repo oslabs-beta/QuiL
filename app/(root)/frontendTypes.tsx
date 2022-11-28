ce8afab682ec1e992201757a092423744be121ec
btran946 <105252771+btran946@users.noreply.github.com>
2022-11-28 12:39:35 -0800

    if implemented, cypress will now have some unit tests

--- app/(root)/frontendTypes.tsx
+++ app/(root)/frontendTypes.tsx
@@ -1,7 +1,11 @@
 import { type } from "os";
 import { Node, Edge, NodeChange, EdgeChange } from "reactflow";
 import { StringMappingType } from "typescript";
-import { nodeShape, ResolverStrings, SchemaType } from "../../server/types";
+import {
+  nodeShape,
+  ResolverStrings,
+  SingleSchemaType,
+} from "../../server/types";
 
 export type DisplayContainerProps = {
   displayMode: string;
@@ -66,7 +70,7 @@ export type resQL = {
 export type getAllData = {
   nodes: nodeShape[];
   resolvers: ResolverStrings[];
-  schemas: [SchemaType];
+  schemas: [SingleSchemaType];
 };
 
 export type nodes = node[];
@@ -108,16 +112,16 @@ export type MainContainerProps = {
   initialNodes: Node[];
   initialEdges: Edge[];
   data: resQL;
-}
+};
 
 export type userObj = {
-  [k: string]: string
-}
+  [k: string]: string;
+};
 
 export type inputObj = {
-  name: string
-}
+  name: string;
+};
 
 export type loggedUser = {
-  [k: string]: any
-}
+  [k: string]: any;
+};
