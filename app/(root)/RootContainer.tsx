47386d2b7383ffeecb709a565a1ce1b533a0230b
btran946 <105252771+btran946@users.noreply.github.com>
2022-11-28 08:11:54 -0800

    Cypress is WORKING

--- app/(root)/RootContainer.tsx
+++ app/(root)/RootContainer.tsx
@@ -1,10 +1,10 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { motion } from 'framer-motion';
-import jwt_decode from 'jwt-decode';
-import router from 'next/router';
-import Register from './Register/Register';
+"use client";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
+import jwt_decode from "jwt-decode";
+import router from "next/router";
+import Register from "./Register/Register";
 
 const RootContainer = () => {
   const [initialURI, setInitialURI] = useState<string>(null);
@@ -40,15 +40,16 @@ const RootContainer = () => {
   // }, []);
 
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
+    <div className='hero min-h-screen bg-base-200'>
+      <div className='hero-content flex-col lg:flex-row-reverse'>
+        <div className='text-center lg:text-left'>
           <motion.h1
             initial={{ opacity: 0, y: -30 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 1 }}
             exit={{ opacity: 0, y: -30 }}
-            className="text-5xl font-bold"
+            className='text-5xl font-bold'
+            data-cy='root-h1'
           >
             QuiL
           </motion.h1>
@@ -57,7 +58,8 @@ const RootContainer = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.5 }}
             exit={{ opacity: 0, y: -30 }}
-            className="py-6"
+            className='py-6'
+            data-cy='root-p'
           >
             QuiL is a developer tool used to visualize an exisiting relational
             database and generate the GraphQL schemas & resolvers for that data
@@ -70,52 +72,70 @@ const RootContainer = () => {
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.8, delay: 2 }}
           exit={{ opacity: 0.5, x: 30 }}
-          className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100"
+          className='card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100'
         >
-          <div className="card-body">
-            {(loggedUser) ? (<h1>Welcome {5 + 5}</h1>) : (
-            <div className='form-control justify-end w-full min-w-full'>
-              <button className="btn btn-primary min-w-1/2" onClick={() => router.push("/Login")}>Login</button> 
-              <button className="btn btn-primary min-w-1/2" onClick={() => router.push("/Register")}>Register</button>
-            </div>)}
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">URI</span>
+          <div className='card-body'>
+            {loggedUser ? (
+              <h1>Welcome {5 + 5}</h1>
+            ) : (
+              <div className='form-control justify-end w-full min-w-full'>
+                <button
+                  className='btn btn-primary min-w-1/2'
+                  onClick={() => router.push("/Login")}
+                  data-cy='root-login'
+                >
+                  Login
+                </button>
+                <button
+                  className='btn btn-primary min-w-1/2'
+                  onClick={() => router.push("/Register")}
+                  data-cy='root-register'
+                >
+                  Register
+                </button>
+              </div>
+            )}
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text'>URI</span>
               </label>
               <input
-                type="text"
+                type='text'
                 disabled={sampleURI ? true : false}
-                placeholder="Enter URI here or choose a sample"
-                className="input input-bordered"
+                placeholder='Enter URI here or choose a sample'
+                className='input input-bordered'
                 onChange={handleUserURI}
+                data-cy='root-uri-input'
               />
             </div>
-            <div className="form-control w-full max-w-xs">
-              <label className="label">
-                <span className="label-text">Sample Database</span>
+            <div className='form-control w-full max-w-xs'>
+              <label className='label'>
+                <span className='label-text'>Sample Database</span>
               </label>
               <select
                 onChange={handleSampleURI}
                 disabled={initialURI ? true : false}
-                className="select select-bordered"
+                className='select select-bordered'
+                data-cy='select-sample-db'
               >
                 <option disabled selected>
                   Pick one
                 </option>
-                <option value="postgres://lkdxllvk:GTIkPygxpPOx0ZVNJ3luQHEfApEIJekP@heffalump.db.elephantsql.com/lkdxllvk">
+                <option value='postgres://lkdxllvk:GTIkPygxpPOx0ZVNJ3luQHEfApEIJekP@heffalump.db.elephantsql.com/lkdxllvk'>
                   Star Wars
                 </option>
-                <option value="postgres://nsjouiot:4nVVHLiARTADoIiwArtQLG-HfkhQR03k@peanut.db.elephantsql.com/nsjouiot">
+                <option value='postgres://nsjouiot:4nVVHLiARTADoIiwArtQLG-HfkhQR03k@peanut.db.elephantsql.com/nsjouiot'>
                   Quitr
                 </option>
               </select>
             </div>
           </div>
-          <div className="form-control mt-6">
+          <div className='form-control mt-6'>
             <button
               disabled={initialURI || sampleURI ? false : true}
               onClick={handleLaunch}
-              className="btn btn-primary"
+              className='btn btn-primary'
+              data-cy='root-launch'
             >
               Launch
             </button>
