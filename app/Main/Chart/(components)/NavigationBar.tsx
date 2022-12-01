55bfb8fd202b6f85dad75d19da906a39c9a2a606
btran946 <105252771+btran946@users.noreply.github.com>
2022-12-01 07:09:19 -0800

    merging with dev to pull logo

--- app/Main/Chart/(components)/NavigationBar.tsx
+++ app/Main/Chart/(components)/NavigationBar.tsx
@@ -1,12 +1,13 @@
-'use client';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
-import { useSearchParams } from 'next/navigation';
-import { NavigationBarProps } from '../../../(root)/frontendTypes';
-import test from 'node:test';
-import { setSyntheticLeadingComments } from 'typescript';
-import quil from './quil.png';
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { NavigationBarProps } from "../../../(root)/frontendTypes";
+import test from "node:test";
+import { setSyntheticLeadingComments } from "typescript";
+import quil from "./quil.png";
+import { motion } from "framer-motion";
 const NavigationBar = ({
   userJWT,
   handleSetTheme,
@@ -15,72 +16,87 @@ const NavigationBar = ({
 }: NavigationBarProps): JSX.Element => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const URIfromRoot = searchParams.get('URI');
+  const URIfromRoot = searchParams.get("URI");
   const [uriParam, setUriParam] = useState(URIfromRoot);
 
   return (
-    <div className="navbar bg-base-200" data-cy="nav-bar">
-      <div className="navbar">
-        <div className="flex-1">
+    <motion.div
+      whileHover={{ backgroundColor: "accent", scale: 1.01 }}
+      className='navbar'
+      data-cy='nav-bar'
+    >
+      <div className='navbar'>
+        <div className='flex-1'>
           <img
-            src="https://files.slack.com/files-pri/T03N6QNVCES-F04D7PJ2GHZ/quillogo_2.png"
-            alt=""
+            src='https://files.slack.com/files-pri/T03N6QNVCES-F04D7PJ2GHZ/quillogo_2.png'
+            alt=''
           />
         </div>
-        <div className="flex-none">
-          <div className="btn-group flex space-x-1 font-mono">
-            <button className="btn btn-primary" onClick={() => mainPageMode()}>
+        <div className='flex-none'>
+          <div className='btn-group flex space-x-1 font-mono'>
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              className='btn btn-primary'
+              onClick={() => mainPageMode()}
+            >
               Main
-            </button>
+            </motion.button>
             {userJWT ? (
-              <button
-                className="btn btn-secondary"
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                className='btn btn-secondary'
                 onClick={() => {
-                  window.localStorage.removeItem('token');
+                  window.localStorage.removeItem("token");
                   window.location.reload();
                 }}
               >
                 Log Out
-              </button>
+              </motion.button>
             ) : (
               <>
-                <button
-                  className="btn btn-secondary"
+                <motion.button
+                  whileHover={{ scale: 1.1 }}
+                  className='btn btn-secondary'
                   onClick={() => {
-                    router.push('/Login');
+                    router.push("/Login");
                   }}
                 >
                   Login
-                </button>
-                <button
-                  className="btn btn-accent"
-                  onClick={() => router.push('/Register')}
+                </motion.button>
+                <motion.button
+                  whileHover={{ scale: 1.1 }}
+                  className='btn btn-accent'
+                  onClick={() => router.push("/Register")}
                 >
                   Register
-                </button>
+                </motion.button>
               </>
             )}
-            <button className="btn" onClick={() => aboutPageMode()}>
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              className='btn'
+              onClick={() => aboutPageMode()}
+            >
               About
-            </button>
+            </motion.button>
             <select
               onChange={(e: any) => handleSetTheme(e.target.value)}
-              className="select bg-neutral-content w-1/3 max-w-xs text-base-300 mr-9"
+              className='select bg-neutral-content w-1/3 max-w-xs text-base-300 mr-9'
             >
               <option disabled selected>
                 Theme
               </option>
-              <option value="light">light</option>
-              <option value="night">night</option>
-              <option value="retro">retro</option>
-              <option value="cyberpunk">cyberpunk</option>
-              <option value="synthwave">synthwave</option>
-              <option value="pastel">pastel</option>
+              <option value='light'>light</option>
+              <option value='night'>night</option>
+              <option value='retro'>retro</option>
+              <option value='cyberpunk'>cyberpunk</option>
+              <option value='synthwave'>synthwave</option>
+              <option value='pastel'>pastel</option>
             </select>
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
